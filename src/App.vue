28f2d9f52956dfa13ebba309fc017af2579f949b
liuqing <1534562543@qq.com>
2020-07-18 04:09:04 +0800

    清理项目

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -17,3 +13,9 @@
   color #2c3e50
   margin-top 60px
 </style>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
